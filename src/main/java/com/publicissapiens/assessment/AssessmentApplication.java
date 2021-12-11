a354e489af01f3c78ae43733e39c0c68aaf93705
nithin babu <nithinbabu265@gmail.com>
2021-12-11 17:45:09 +0530

    testing trigger

--- src/main/java/com/publicissapiens/assessment/AssessmentApplication.java
+++ src/main/java/com/publicissapiens/assessment/AssessmentApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableAutoConfiguration
 public class AssessmentApplication {
-	//TODO application yet to start
+	//TODO application yet to start- once again
 	public static void main(String[] args) {
 		SpringApplication.run(AssessmentApplication.class, args);
 	}
