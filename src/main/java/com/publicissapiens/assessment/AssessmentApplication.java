f6049bce68750cbfa434abdd3404347c6b340e36
nithin babu <nithinbabu265@gmail.com>
2021-12-11 17:33:36 +0530

    second commit for trigger testing

--- src/main/java/com/publicissapiens/assessment/AssessmentApplication.java
+++ src/main/java/com/publicissapiens/assessment/AssessmentApplication.java
@@ -1,11 +1,13 @@
 package com.publicissapiens.assessment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class AssessmentApplication {
-
+	//TODO application yet to start
 	public static void main(String[] args) {
 		SpringApplication.run(AssessmentApplication.class, args);
 	}
